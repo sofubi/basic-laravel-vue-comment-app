577f2d9d40221df5e186222372ad2790a5d6d252
sofubi <thomaslawton91@gmail.com>
2020-11-24 21:03:17 -0500

    Finishes basic controller for Reply Finishes basic controller for SubReply

--- app/Http/Controllers/ReplyController.php
+++ app/Http/Controllers/ReplyController.php
@@ -2,21 +2,13 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Reply;
+use App\Models\Comment;
+use Exception;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
 
 class ReplyController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
-    {
-        //
-    }
-
     /**
      * Store a newly created resource in storage.
      *
@@ -25,17 +17,17 @@ class ReplyController extends Controller
      */
     public function store(Request $request)
     {
-        //
-    }
-
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\Models\Reply  $reply
-     * @return \Illuminate\Http\Response
-     */
-    public function show(Reply $reply)
-    {
-        //
+        try {
+            Comment::find($request->comment_id)
+                ->replies()
+                ->create([
+                    'content' => $request->reply
+                ]);
+        } catch (Exception $e) {
+            Log::error("Reply Storage Exception: \n $e");
+            // For the sake of fun and to move quickly I'll use error 41
+            // https://developer.mozilla.org/en-US/docs/Web/HTTP/Status/418
+            return response("An unknow error was encountered", 418);
+        }
     }
 }
--- app/Http/Controllers/SubReplyController.php
+++ app/Http/Controllers/SubReplyController.php
@@ -2,21 +2,13 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\SubReply;
+use App\Models\Reply;
+use Exception;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
 
 class SubReplyController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
-    {
-        //
-    }
-
     /**
      * Store a newly created resource in storage.
      *
@@ -25,17 +17,18 @@ class SubReplyController extends Controller
      */
     public function store(Request $request)
     {
-        //
-    }
-
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\Models\SubReply  $subReply
-     * @return \Illuminate\Http\Response
-     */
-    public function show(SubReply $subReply)
-    {
-        //
+        try {
+            Reply::find($request->reply_id)
+                ->sub_replies()
+                ->create([
+                    'content' => $request->sub_reply
+                ]);
+        } catch (Exception $e) {
+            Log::error("SubReply Storage Exception: \n $e");
+            // For the sake of fun and to move quickly I'll use error 41
+            // https://developer.mozilla.org/en-US/docs/Web/HTTP/Status/418
+            return response("An unknow error was encountered", 418);
+        }
+        
     }
 }
